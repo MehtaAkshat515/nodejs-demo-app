4f3f15b3db0bf81541f44590a17b45979a3611e1
MehtaAkshat515 <102755156+MehtaAkshat515@users.noreply.github.com>
2022-04-05 11:20:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
 import express from 'express';
 import routes from './source/routes/route.js';
 
-// vary these constants according to where you are running (GCS, VMs, K8S) and how many VMs you have, if that's the lesson)
-// to do: this should be a command-line parameter that lets the app know how it is running
-//const arrNodes = [ "localhost" ]                                    // for testing on GCS
+// for testing on GCS
 const arrNodes = [ process.env.NODE_SVC_PUBLIC_SERVICE_HOST  ];       //  use this for K8S
 
 // constant variables 
@@ -12,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 30002;
 const HOST = '0.0.0.0';
 
-// body parser setup for Express v4.16.0 and higher
+
 app.use(express.json());
 app.use(express.urlencoded(
 	{
@@ -20,8 +18,7 @@ app.use(express.urlencoded(
 	}
 ));
 
-// We'll use our routes function that we setup and imported above
-// and then pass it to our app Express 
+
 routes(app);
 
 // Setting the server to listen at port 3000
@@ -30,9 +27,7 @@ app.listen(PORT, HOST, function () {
 });
 
 function buildURL (strLevel) {
-  // what is the formula for which node to call? 
-  // given x levels and n nodes
-  // x % n  where x>n, else n 
+
   let intCurrLevel = parseInt(strLevel);
   let nextLevel = intCurrLevel - 1;
   let numNodes = arrNodes.length; // to be derived from arrNodes
